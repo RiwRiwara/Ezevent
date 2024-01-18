642c2be74ce0bb3a70110ac0cd73ac7c916bc7a9
RiwRiwara <awirut2629@gmail.com>
2024-01-18 22:56:28 +0700

    add frontend asset

---
+++ resources/views/components/import-script-link.blade.php
@@ -0,0 +1,10 @@
+<!-- <script src="{{ env('RESOURECE_ASSET_URL') }}/js/app-nGfMv9XH.js"></script> -->
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js" integrity="sha256-xLD7nhI62fcsEZK2/v8LsBcb4lG7dgULkuXoXB/j91c=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="{{ env('RESOURECE_ASSET_URL') }}/js/mdb.umd.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js" integrity="sha256-xLD7nhI62fcsEZK2/v8LsBcb4lG7dgULkuXoXB/j91c=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+<script src="{{ env('RESOURECE_ASSET_URL') }}/snackbar/dist/snackbar.min.js"></script>
+<script src="{{ env('RESOURECE_ASSET_URL') }}/snackbar/dist/snackbar.js"></script>
+<script src="{{ env('RESOURECE_ASSET_URL') }}/js/base.js"></script>
