2a1e64fb5f1a57d2ae48bf656cad1abe5bc4e551
PeterSmoke01 <toaaz1234@hotmail.com>
2024-01-20 18:10:20 +0700

    LoginUnfinnish

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
-    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8" style="background-color: #44A9C9;">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+        <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" >
         <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">EZEVENT</h2>
     </div>
 
