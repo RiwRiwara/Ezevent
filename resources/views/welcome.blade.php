3ca1e6775908cab923b4ae7729561f7e1a6e1f05
PeterSmoke01 <toaaz1234@hotmail.com>
2024-01-21 01:33:56 +0700

    LoginFinal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,100 @@
 <x-guest-layout>
-    <div class="container">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis iure, qui eos optio quaerat fugiat possimus dicta, ducimus impedit veritatis reprehenderit cum dolores aspernatur quia officia amet iste quam.
+<div class="flex bg-gray-100 text-gray-900">
+        <aside class="flex h-screen w-20 flex-col items-center border-r border-gray-200 bg-neutral-7">
+            <button class="mt-2 rounded-full bg-gray-100">
+                <img class="h-10 w-10 rounded-full" src="https://avatars.githubusercontent.com/u/35387401?v=4" alt="" />
+            </button>
+            <nav class="flex flex-1 flex-col gap-y-4 pt-4">
+                <a href="#" class="text-white group relative rounded-xl p-2 hover:bg-gray-50 bg-neutral-8">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                        class="bi bi-inbox" viewBox="0 0 16 16">
+                        <path
+                            d="M4.98 4a.5.5 0 0 0-.39.188L1.54 8H6a.5.5 0 0 1 .5.5 1.5 1.5 0 1 0 3 0A.5.5 0 0 1 10 8h4.46l-3.05-3.812A.5.5 0 0 0 11.02 4zm9.954 5H10.45a2.5 2.5 0 0 1-4.9 0H1.066l.32 2.562a.5.5 0 0 0 .497.438h12.234a.5.5 0 0 0 .496-.438zM3.809 3.563A1.5 1.5 0 0 1 4.981 3h6.038a1.5 1.5 0 0 1 1.172.563l3.7 4.625a.5.5 0 0 1 .105.374l-.39 3.124A1.5 1.5 0 0 1 14.117 13H1.883a1.5 1.5 0 0 1-1.489-1.314l-.39-3.124a.5.5 0 0 1 .106-.374z" />
+                    </svg>
+                    <div class="absolute inset-y-0 left-12 hidden items-center group-hover:flex">
+                        <div
+                            class="relative whitespace-nowrap rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 drop-shadow-lg">
+                            <div class="absolute inset-0 -left-1 flex items-center">
+                                <div class="h-2 w-2 rotate-45 bg-white"></div>
+                            </div>
+                            Your Events
+                        </div>
+                    </div>
+                </a>
+                <a href="#" class="text-white group relative rounded-xl p-2 hover:bg-gray-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                        class="bi bi-calendar-event" viewBox="0 0 16 16">
+                        <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z" />
+                        <path
+                            d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z" />
+                    </svg>
+
+                    <div class="absolute inset-y-0 left-12 hidden items-center group-hover:flex">
+                        <div
+                            class="relative whitespace-nowrap rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 drop-shadow-lg">
+                            <div class="absolute inset-0 -left-1 flex items-center">
+                                <div class="h-2 w-2 rotate-45 bg-white"></div>
+                            </div>
+                            Calendar
+                        </div>
+                    </div>
+                </a>
+                <a href="#" class="text-white group relative rounded-xl p-2 hover:bg-gray-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                        class="bi bi-people" viewBox="0 0 16 16">
+                        <path
+                            d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
+                    </svg>
+
+                    <div class="absolute inset-y-0 left-12 hidden items-center group-hover:flex">
+                        <div
+                            class="relative whitespace-nowrap rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 drop-shadow-lg">
+                            <div class="absolute inset-0 -left-1 flex items-center">
+                                <div class="h-2 w-2 rotate-45 bg-white"></div>
+                            </div>
+                            Color Scheme
+                        </div>
+                    </div>
+                </a>
+                <a href="#" class="text-white group relative rounded-xl p-2 hover:bg-gray-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                        class="bi bi-star" viewBox="0 0 16 16">
+                        <path
+                            d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z" />
+                    </svg>
+
+                    <div class="absolute inset-y-0 left-12 hidden items-center group-hover:flex">
+                        <div
+                            class="relative whitespace-nowrap rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 drop-shadow-lg">
+                            <div class="absolute inset-0 -left-1 flex items-center">
+                                <div class="h-2 w-2 rotate-45 bg-white"></div>
+                            </div>
+                            Color Scheme
+                        </div>
+                    </div>
+                </a>
+            </nav>
+
+            <div class="flex flex-col items-center gap-y-4 py-10">
+                <a href="#" class="text-white group relative rounded-xl p-2 hover:bg-gray-50">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                        class="bi bi-door-closed" viewBox="0 0 16 16">
+                        <path
+                            d="M3 2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v13h1.5a.5.5 0 0 1 0 1h-13a.5.5 0 0 1 0-1H3zm1 13h8V2H4z" />
+                        <path d="M9 9a1 1 0 1 0 2 0 1 1 0 0 0-2 0" />
+                    </svg>
+
+                    <div class="absolute inset-y-0 left-12 hidden items-center group-hover:flex">
+                        <div
+                            class="relative whitespace-nowrap rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 drop-shadow-lg">
+                            <div class="absolute inset-0 -left-1 flex items-center">
+                                <div class="h-2 w-2 rotate-45 bg-white"></div>
+                            </div>
+                            Logout
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </aside>
     </div>
-    <h1>Hello World</h1>
 </x-guest-layout>
