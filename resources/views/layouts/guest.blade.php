d0a9dc4172e33bf98b57f41d6a13526aac8e310f
3422 <sirawit.pongsawet@hotmail.com>
2024-01-26 14:43:27 +0700

    แก้ SideBar

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,14 +5,16 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.css"  rel="stylesheet" />
 
     <title>{{ config('app.name', 'Laravel') }}</title>
     @vite('resources/css/app.css')
-
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
     {{ $slot }}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
+
 </body>
 @vite('resources/js/app.js')
 
