39c0bf62af39a89a21addb15328bf60c7a6db4bb
3422 <sirawit.pongsawet@hotmail.com>
2024-02-20 15:28:37 +0700

    แก้เป็นresponsive

--- resources/views/guest/eventpage.blade.php
+++ resources/views/guest/eventpage.blade.php
@@ -5,13 +5,14 @@
         </h2>
     </x-slot>
 
-    <!-- <h1 class="text-3xl px-20 py-5 text-left text-primary-5">Ezevent</h1> -->
-    <h2 class="text-2xl px-20 py-5 text-left text-gray-9">Junior Architecture</h2>
-    <!-- ใส่ Nav Bar ตรงเน้ -->
-    <h3 class="text-2xl px-20 text-left text-gray-9 mx-20">{{__('page.overview')}}</h3>
-    <div class="container px-10 mx-auto pt-8 grid gap-20 grid-cols-2">
-        <!-- left-content -->
-        <div class="text-lg border border-neutral-9 px-10 py-10 grid grid-cols-1">
+    <h2 class="text-2xl px-4 md:px-20 py-5 text-left text-gray-9">Junior Architecture</h2>
+
+    
+
+    <h3 class="text-2xl px-4 md:px-20 text-left text-gray-9">{{__('page.overview')}}</h3>
+    <div class="container px-4 mx-auto pt-8 grid gap-4 md:gap-20 grid-cols-1 md:grid-cols-2">
+        
+        <div class="text-lg border border-neutral-9 px-4 md:px-10 py-10 grid grid-cols-1">
             <div class="border-b border-neutral-9">
                 <p class="text-gray-9">{{__('page.total_participants')}}</p>
                 <p class="text-gray-9">2/25 {{__('page.person')}}</p>
@@ -21,42 +22,43 @@
                 <p class="text-gray-9">0/25 {{__('page.person')}}</p>
             </div>
         </div>
-        <!-- right-content -->
-        <div class="border border-neutral-9 px-10 py-5">
+        
+        <div class="border border-neutral-9 px-4 md:px-10 py-5">
             <div class="mt-2">
                 <p class="text-lg text-gray-9 text-center">{{__('page.nowphase')}} : {{__('page.in_progress')}}</p>
-                <!--radio 1 column 2 radio-->
+                
                 <ul class="mt-5 w-full text-lg text-gray-9 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                     <li class="w-full dark:border-gray-600">
-                        <div class="grid grid-cols-2 px-20">
-                            <div class="px-10">
+                        <div class="grid grid-cols-2 px-4 md:px-20">
+                            <div class="px-4 md:px-10">
                                 <input checked id="upcoming" name="radio" type="radio" value="" class="w-4 h-4 text-primary-4 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                 <label for="upcoming" class="text-lg text-gray-9 dark:text-gray-6">{{__('page.upcome')}}</label>
                             </div>
-                            <div class="px-10">
+                            <div class="px-4 md:px-10">
                                 <input id="progress" name="radio" type="radio" value="" class="w-4 h-4 text-primary-4 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                 <label for="progress" class="text-lg text-gray-9 dark:text-gray-6">{{__('page.in_progress')}}</label>
                             </div>
-                            <div class="px-10">
+                            <div class="px-4 md:px-10">
                                 <input id="reviewing" name="radio" type="radio" value="" class="w-4 h-4 text-primary-4 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                 <label for="reviewing" class="text-lg text-gray-9 dark:text-gray-6">{{__('page.review')}}</label>
                             </div>
-                            <div class="px-10">
+                            <div class="px-4 md:px-10">
                                 <input id="complete" name="radio" type="radio" value="" class="w-4 h-4 text-primary-4 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                                 <label for="complete" class="text-lg text-gray-9 dark:text-gray-6">{{__('page.complete')}}</label>
                             </div>
-
                         </div>
+                    </li>
+                </ul>
             </div>
             <div class="mt-5 flex justify-center">
-                <button type="submit" class="block w-80 border border-neutral-8 rounded-md bg-white text-center text-xl text-neutral-8 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{{__('page.save_phase')}}</button>
+                <button type="submit" class="block w-80 md:w-auto border border-neutral-8 rounded-md bg-white px-3.5 text-center text-xl text-neutral-8 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{{__('page.save_phase')}}</button>
             </div>
             <div class="mt-5 flex justify-center">
-                <button type="submit" class="block w-80 border border-neutral-8 rounded-md bg-white px-3.5 py-2.5 text-center text-2xl text-neutral-8 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{{__('page.create_qr')}}</button>
+                <button type="submit" class="block w-80 md:w-auto border border-neutral-8 rounded-md bg-white px-3.5 py-2 text-center text-2xl text-neutral-8 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{{__('page.create_qr')}}</button>
             </div>
         </div>
     </div>
-    <div class="relative overflow-x-auto px-20">
+    <div class="relative overflow-x-auto px-4 md:px-20">
         <h3 class="text-2xl py-10 text-gray-9">{{__('page.check')}}</h3>
         <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead class="text-lg text-gray-9 font-semibold dark:bg-gray-700 dark:text-gray-400">
@@ -124,6 +126,4 @@
             </tbody>
         </table>
     </div>
-
 </x-guest-layout>
-```
