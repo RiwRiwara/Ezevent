f046976da4dd8cd030cfc717765082fadd84d92e
RiwRiwara <awirut2629@gmail.com>
2024-03-30 02:49:34 +0700

    Update authen api

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -16,5 +16,6 @@ return [
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'invalid_role' => 'You do not have the required role to access this page.',
 
 ];
