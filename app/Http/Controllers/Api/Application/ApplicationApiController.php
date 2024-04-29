07ddf04b7f5f00ddc3fb7d02feb04a75f77be9a2
RiwRiwara <awirut2629@gmail.com>
2024-04-29 22:52:23 +0700

    Add getMyApplication API endpoint and remove StaffApplication controller

--- app/Http/Controllers/Api/Application/ApplicationApiController.php
+++ app/Http/Controllers/Api/Application/ApplicationApiController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api\Application;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\Event\ParticipantApplicationRequest;
-use App\Http\Resources\ApplicationResource;
 
 class ApplicationApiController extends Controller
 {
@@ -28,5 +27,26 @@ class ApplicationApiController extends Controller
     {
 
         return (new ParticipantApplication())->participantApplication($request);
+    }
+
+
+    /**
+     * Get my applications [T]
+     * 
+     * Retrieves all applications for the current user and returns an array of application objects.
+     * 
+     * ดึงข้อมูลการสมัครของฉันทั้งหมดและส่งคืนอาร์เรย์ของอ็อบเจ็กต์การสมัคร
+     * 
+     * @param Request $request The request object.
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     * 
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     * 
+     */
+
+    public function getMyApplication(Request $request)
+    {
+        return (new GetMyApplication())->getMyApplication($request);
     }
 }
