653b2fbdcb2dcfb296340baa08ea8e94d7d6ae1a
xKollawaTx <93873084+xKollawaTx@users.noreply.github.com>
2024-05-07 18:10:53 +0700

    userView&Edit

--- resources/views/admin/users-dashboard.blade.php
+++ resources/views/admin/users-dashboard.blade.php
@@ -83,7 +83,7 @@
                                 {{$user->created_at}}
                             </td>
                             <td class="px-4 py-2 text-right">
-                                <a type="link" href="" class="text-neutral-8">
+                            <a href="{{route('user-profile', ['user_id' => $user->user_id])}}" class="text-neutral-8">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                     </svg>
